f47f699bce31189a775531665b147c3984e72df2
Erick Bleicher <erick.b1@aluno.ifsc.edu.br>
2022-02-24 17:02:54 -0300

    Inicio do projeto - Imprimindo meu nome com classe Scanner

---
+++ src/erickbleicher/ErickBleicher.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package erickbleicher;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author IFSC
+ */
+public class ErickBleicher {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    Scanner leia = new Scanner(System.in);
+        System.out.println("Entre com seu nome: ");
+        String nome = leia.nextLine();
+        System.out.println(nome+" Bem vindo ao GitHub ");
+        
+}
+}
